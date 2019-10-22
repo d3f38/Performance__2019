c8e6be0c5159c18b584d9928f2dbe42475d6c1a6
Aleksandr Bylkov <abylkov.kameleoon.com>
2019-10-22 15:19:50 +0300

    delete $ carousel

--- js/main.js
+++ js/main.js
@@ -140,12 +140,12 @@ selectOptions.forEach(o => {
 
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    var carousel = $("#carousel");
+//     var carousel = $("#carousel");
 
-    carousel.owlCarousel();
-});
+//     carousel.owlCarousel();
+// });
 
 function status(e) {
     return 200 <= e.status && e.status < 300 ? Promise.resolve(e) : Promise.reject(new Error(e.statusText))
@@ -158,6 +158,10 @@ function json(e) {
 
 
 document.addEventListener("DOMContentLoaded", function () {
+    var carousel = document.querySelector("#carousel");
+
+    carousel.owlCarousel();
+
     const buttonsContainer = document.querySelector(".buttons-wrap");
     const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
     setTimeout(function() {

8d3b0d0bc4cba1355017f1a1551fa0e59aa2c5c4
Aleksandr Bylkov <abylkov.kameleoon.com>
2019-10-24 19:24:06 +0300

    asd

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const buttonsContainer = document.querySelector(".buttons-wrap");
     const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
-    setTimeout(function() {
+
         const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
         const purchaseListContainer = document.createElement('div');
         const purchaseListTitle = document.createElement('p');
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
             fridgeInfoContainer.replaceWith(purchaseListContainer)
             buttonsContainer.style.display = "none";
         }
-    }, 500)
+
 
     document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
         document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
